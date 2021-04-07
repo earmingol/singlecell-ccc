0dc18ddbe60c458938fc8d8f7bde10a63c53cf87
Erick Armingol <earmingol14@gmail.com>
2021-04-06 21:58:34 -0700

    Changed NMF package in packages.r

--- packages.r
+++ packages.r
@@ -1,6 +1,5 @@
-source('http://renozao.github.io/repotools/install.R')
-library(repotools)
-install.pkgs('NMF')
+Sys.setenv(R_REMOTES_NO_ERRORS_FROM_WARNINGS=TRUE)
+devtools::install_github("renozao/NMF@devel")
 devtools::install_github("jokergoo/circlize")
 devtools::install_github("jokergoo/ComplexHeatmap")
 devtools::install_github("sqjin/CellChat")
