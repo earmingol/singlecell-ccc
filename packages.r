1e662cba4a0f967633bd454d4dfbfbaf8db47ea8
Erick Armingol <earmingol14@gmail.com>
2021-04-14 20:25:37 -0700

    Added seuratdisk + harmony

--- packages.r
+++ packages.r
@@ -1,6 +1,8 @@
 options(repos = c(CRAN = "http://cran.rstudio.com"))
 install.packages('Seurat')
 Sys.setenv(R_REMOTES_NO_ERRORS_FROM_WARNINGS=TRUE)
+devtools::install_github("mojaveazure/seurat-disk")
+devtools::install_github("immunogenomics/harmony")
 devtools::install_github("renozao/NMF@devel")
 devtools::install_github("jokergoo/circlize")
 devtools::install_github("jokergoo/ComplexHeatmap")
